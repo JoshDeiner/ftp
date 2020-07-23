91fa1358dd7b16a7ed40f4eb79985bde061f6071
Josh Deiner <joshdeiner75@gmail.com>
2020-07-23 19:36:01 -0400

    add upload and delete to class

--- python_runner.py
+++ python_runner.py
@@ -1,46 +1,71 @@
-''' python runner for ftp   '''
+""" python runner for ftp   """
 
-import ftplibdd
-FTP_HOST = "ftp.dlptest.com"
-FTP_USER = "dlpuser@dlptest.com"
-FTP_PASS = "SzMf7rTE4pCrf9dV286GuNe4N"
+import ftplib
 
 class FtpParser:
-  def __init__(self):
-    self._host = "host"
-    self.user = "name"
-    self.pw = "imp" 
+    def __init__(self):
+        self._host = "ftp://ftp.dlptest.com/"
+        self._user = "dlpuser@dlptest.com"
+        self._pw = "SzMf7rTE4pCrf9dV286GuNe4N"
 
-  def connect_to_ftp_server(self):
-      # connect to the FTP server
-      ftp_base = ftplib.FTP(FTP_HOST, FTP_USER, FTP_PASS)
-      # force UTF-8 encoding
-      ftp_base.encoding = "utf-8"
-      return ftp_base
+    @property
+    def host(self):
+        return self._host
 
-  def upload_file(self):
-      # local file name you want to upload
-      ftp = self.connect_to_ftp_server()
-      filename = "some_file.txt"
-      #reading the local file in binary mode
-      with open(filename, "rb") as file:
-         # use FTP's STOR command to upload the file
-         ftp.storbinary(f"STOR {filename}", file)
-      ftp.dir()
-      self.close_conn()
+    @property
+    def user(self):
+        return self._user
 
-  def close_conn(self):
-     # quit and close the connection
-     ftp = self.connect_to_ftp_server()
-     ftp.quit()
+    def upload_file(self):
+        FTP_HOST = "ftp.dlptest.com"
+        FTP_USER = "dlpuser@dlptest.com"
+        FTP_PASS = "SzMf7rTE4pCrf9dV286GuNe4N"
+        # connect to the FTP server
+        ftp = ftplib.FTP(FTP_HOST, FTP_USER, FTP_PASS)
+        # force UTF-8 encoding
+        ftp.encoding = "utf-8"
+        # local file name you want to upload
+        filename = "some_file.txt"
+        with open(filename, "rb") as file:
+            # use FTP's STOR command to upload the file
+            ftp.storbinary(f"STOR {filename}", file)
+        ftp.dir()
 
-  def download_files(self):
-     # the name of file you want to download from the FTP server
-     ftp = self.connect_to_ftp_server()
-     filename = "some_file.txt"
-     with open(filename, "wb") as file:
-       # use FTP's RETR command to download the file
-       ftp.retrbinary(f"RETR {filename}", file.write)
 
-if __name__ == '__main__':
-  print('dude')
+    @property
+    def pw(self):
+        return self._pw
+
+    def connect_to_ftp_server(self):
+        # connect to the FTP server
+        ftp_base = FTP(self.host, self.user, self.pw, '')
+        # force UTF-8 encoding
+        ftp_base.encoding = "utf-8"
+        return ftp_base
+
+    def close_conn(self):
+        # quit and close the connection
+        ftp = self.connect_to_ftp_server()
+        ftp.quit()
+
+    def download_files(self):
+        FTP_HOST = "ftp.dlptest.com"
+        FTP_USER = "dlpuser@dlptest.com"
+        FTP_PASS = "SzMf7rTE4pCrf9dV286GuNe4N"
+        # connect to the FTP server
+        ftp = ftplib.FTP(FTP_HOST, FTP_USER, FTP_PASS)
+        # force UTF-8 encoding
+        ftp.encoding = "utf-8"
+        # local file name you want to upload
+        filename = "some_file.txt"
+        with open(filename, "wb") as file:
+             ftp.retrbinary(f"RETR {filename}", file.write)
+
+        # quit and close the connection
+        ftp.quit()
+
+if __name__ == "__main__":
+    print("dude")
+    test_class = FtpParser()
+    #test_class.upload_file()
+    test_class.download_files()
