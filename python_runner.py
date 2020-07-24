d1c9735a9c364e312c842af1d627299be0047ddc
Josh Deiner <joshdeiner75@gmail.com>
2020-07-23 21:01:38 -0400

    can connect to azure storage cleaner class structure

--- python_runner.py
+++ python_runner.py
@@ -1,71 +1,123 @@
 """ python runner for ftp   """
+"""
+error handling needs to be added
+unit testing could be very useful(pytest)
+if cant move files directly from servers without downloading in between, need optimization if large scale
+can include partition based on block size
+"""
 
-import ftplib
+import ftplib, os, uuid
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
-class FtpParser:
-    def __init__(self):
-        self._host = "ftp://ftp.dlptest.com/"
-        self._user = "dlpuser@dlptest.com"
-        self._pw = "SzMf7rTE4pCrf9dV286GuNe4N"
+
+class FtpMiddleware:
+    
+    def __str__(self):
+        return ("need to validate whether the transfer suceeded")
 
     @property
     def host(self):
         return self._host
 
+    @host.setter
+    def host(self, host):
+        self._host = host
+
     @property
     def user(self):
         return self._user
 
-    def upload_file(self):
-        FTP_HOST = "ftp.dlptest.com"
-        FTP_USER = "dlpuser@dlptest.com"
-        FTP_PASS = "SzMf7rTE4pCrf9dV286GuNe4N"
+    @user.setter
+    def user(self, user):
+        self._user = user
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, password):
+        self._password = password
+
+    def set_access(self):
+        # hard coded credentials for webpage ftp
+        self.user = "dlpuser@dlptest.com"
+        self.password = "SzMf7rTE4pCrf9dV286GuNe4N"
+        self.host = "ftp.dlptest.com"
+
+    def set_ftp(self):
+        self.set_access()
         # connect to the FTP server
-        ftp = ftplib.FTP(FTP_HOST, FTP_USER, FTP_PASS)
+        ftp = ftplib.FTP(self.host, self.user, self.password)
         # force UTF-8 encoding
         ftp.encoding = "utf-8"
+        return ftp
+
+    def upload_file(self, filename="some_file.txt"):
+        ftp = self.set_ftp()
         # local file name you want to upload
-        filename = "some_file.txt"
         with open(filename, "rb") as file:
             # use FTP's STOR command to upload the file
             ftp.storbinary(f"STOR {filename}", file)
         ftp.dir()
 
+    def upload_folder(self, directory="testdirectory"):
+        ftp = self.set_ftp()
+        ftp.mkd(directory)
 
-    @property
-    def pw(self):
-        return self._pw
+    def connect_azure(self, file_name):
+        '''connect to azure storage account and blob container  '''
+        connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+        blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+        # may want to hardcode container name or use some form data structure to hold container options
+        container_name = "quickstart" + str(uuid.uuid4()) 
+        container_client = blob_service_client.create_container(container_name)
+        # Write text to the file
+        local_path = "./"
+        local_file_name = file_name
+        upload_file_path = os.path.join(local_path, local_file_name)
+        # Create a blob client using the local file name as the name for the blob
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
+        print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
+        # Upload the created file
+        with open(upload_file_path, "rb") as data:
+            blob_client.upload_blob(data)
 
-    def connect_to_ftp_server(self):
-        # connect to the FTP server
-        ftp_base = FTP(self.host, self.user, self.pw, '')
-        # force UTF-8 encoding
-        ftp_base.encoding = "utf-8"
-        return ftp_base
-
-    def close_conn(self):
+    def close_conn(self, ftp):
         # quit and close the connection
-        ftp = self.connect_to_ftp_server()
         ftp.quit()
 
-    def download_files(self):
-        FTP_HOST = "ftp.dlptest.com"
-        FTP_USER = "dlpuser@dlptest.com"
-        FTP_PASS = "SzMf7rTE4pCrf9dV286GuNe4N"
-        # connect to the FTP server
-        ftp = ftplib.FTP(FTP_HOST, FTP_USER, FTP_PASS)
-        # force UTF-8 encoding
-        ftp.encoding = "utf-8"
-        # local file name you want to upload
-        filename = "some_file.txt"
+    def download_files(self, filename="some_file.txt"):
+        ftp = self.set_ftp()
+        # local file name you want to download
         with open(filename, "wb") as file:
-             ftp.retrbinary(f"RETR {filename}", file.write)
-
+            ftp.retrbinary(f"RETR {filename}", file.write)
         # quit and close the connection
         ftp.quit()
+
+    def init_transfer(self):
+        ftp = self.set_ftp()
+        # local file name you want to upload
+        print(ftp)
+        file_names = ['some_file.txt']
+        for i, file_name in enumerate(file_names):
+             self.download_files(file_name)
+             self.connect_azure(file_name)
+             print("if succeeds to delete")
+             # if you find deleting a file interferes with your work process locally and you are using git
+             # you can use git restore
+             os.remove(file_name)
+             # need to return a status code
+             # if succeeds delete file locally in path
+        ftp.dir()
+
 
 if __name__ == "__main__":
-    print("dude")
-    test_class = FtpParser()
-    #test_class.upload_file()
-    test_class.download_files()
+    tc = FtpMiddleware()
+    print(tc)
+    tc.upload_file()
+    #tc.download_files()
+    #tc.upload_folder()
+    #tc.set_access()
+    #tc.connect_azure()
+    #tc.init_transfer()
